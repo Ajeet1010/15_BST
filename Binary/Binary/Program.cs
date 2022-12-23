13cf2f15bc04dff2c3023b4350aebfcad877a56e
ajeet1010 <ajeetpal101999@gmail.com>
2022-12-24 03:13:43 +0530

    added with using size method in Binary Tree

--- Binary/Binary/Program.cs
+++ Binary/Binary/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args) 
         {
             Console.WriteLine("BINARY SEARCH TREE ");
-            Console.WriteLine("1.BST .");
+            Console.WriteLine("1.Creating a binary search node\n2. Creating a binary tree using size\n");
             Console.WriteLine("Enter the option");
             int num = Convert.ToInt32(Console.ReadLine());
             BST<int> binarySearchTree = new BST<int>(56);
@@ -16,6 +16,24 @@
                     binarySearchTree.Insert(70);
                     binarySearchTree.Display();
                     break;
+
+                case 2:
+                    binarySearchTree.Insert(30);
+                    binarySearchTree.Insert(70);
+                    binarySearchTree.Insert(22);
+                    binarySearchTree.Insert(40);
+                    binarySearchTree.Insert(11);
+                    binarySearchTree.Insert(3);
+                    binarySearchTree.Insert(16);
+                    binarySearchTree.Insert(60);
+                    binarySearchTree.Insert(95);
+                    binarySearchTree.Insert(65);
+                    binarySearchTree.Insert(63);
+                    binarySearchTree.Insert(67);
+                    binarySearchTree.Display();
+                    binarySearchTree.GetSize();
+                    break;
+
                 default:
                     Console.WriteLine("Enter valid option!!!");
                     break;
